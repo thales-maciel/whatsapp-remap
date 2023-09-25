6cdfc0d4b41a91fae198ba4059684654f22c90d7
Thales Maciel <thales@thalesmaciel.com>
2023-09-25 10:44:59 -0300

    first refactor 1.1

--- js/content.js
+++ js/content.js
@@ -1,58 +1,78 @@
-// Initialize settings
-let nextChatShortcut = { key: 'j', ctrl: true, alt: false, shift: false };
-let prevChatShortcut = { key: 'k', ctrl: true, alt: false, shift: false };
+// Initialize definitions
+let hashedDefinitions = {
+    'backspace:true:true:true': 'delete',
+    'u:true:true:true': 'markUnread',
+    'm:true:true:true': 'mute',
+    'e:true:true:true': 'archive',
+    'p:true:true:true': 'pin',
+    '/:true:true:false': 'search',
+    'f:true:true:true': 'searchInChat',
+    'n:true:true:false': 'newChat',
+    'n:true:true:true': 'newGroup',
+    'tab:true:true:false': 'nextChat',
+    'tab:true:true:true': 'prevChat',
+    'escape:true:true:true': 'closeChat',
+    'p:true:true:false': 'profile',
+    '.:false:false:true': 'speedUp',
+    ',:false:false:true': 'speedDown',
+    ',:true:true:false': 'settings',
+    'e:true:true:false': 'emojiPanel',
+    'g:true:true:false': 'gifPanel',
+    's:true:true:false': 'stickerPanel',
+    'k:false:true:false': 'extendedSearch',
+    'l:true:true:false': 'lockScreen',
+}
 
-// Function to load settings
-const loadSettings = () => {
-  browser.storage.local.get(['nextChatShortcut', 'prevChatShortcut']).then((result) => {
-    if (result.nextChatShortcut) nextChatShortcut = result.nextChatShortcut;
-    if (result.prevChatShortcut) prevChatShortcut = result.prevChatShortcut;
+// Function to load definitions from browser
+const loadDefinitions = () => {
+  browser.storage.local.get(['hashedDefinitions']).then((result) => {
+    if (result.hashedDefinitions) hashedDefinitions = result.hashedDefinitions;
   });
 };
 
-// Listen for changes in settings
+// Listen for changes in definitions
 browser.storage.onChanged.addListener((changes) => {
-  if (changes.nextChatShortcut) nextChatShortcut = changes.nextChatShortcut.newValue;
-  if (changes.prevChatShortcut) prevChatShortcut = changes.prevChatShortcut.newValue;
+  if (changes.hashedDefinitions) hashedDefinitions = changes.hashedDefinitions.newValue;
 });
 
 // Load initial settings
-loadSettings();
+loadDefinitions();
 
-const nextChatEvent =
-    new KeyboardEvent('keydown', {
-        key: 'Tab',
-        ctrlKey: true,
-        altKey: true
-    });
+const commands = {
+    deleteChat:     new KeyboardEvent('keydown', { key: 'Backspace', ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    markUnread:     new KeyboardEvent('keydown', { key: 'u',         ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    mute:           new KeyboardEvent('keydown', { key: 'm',         ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    archive:        new KeyboardEvent('keydown', { key: 'e',         ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    pin:            new KeyboardEvent('keydown', { key: 'p',         ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    search:         new KeyboardEvent('keydown', { key: '/',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    searchInChat:   new KeyboardEvent('keydown', { key: 'f',         ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    newChat:        new KeyboardEvent('keydown', { key: 'n',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    newGroup:       new KeyboardEvent('keydown', { key: 'n',         ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    nextChat:       new KeyboardEvent('keydown', { key: 'Tab',       ctrlKey: true,  altKey: true,  shiftKey: false }),
+    prevChat:       new KeyboardEvent('keydown', { key: 'Tab',       ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    closeChat:      new KeyboardEvent('keydown', { key: 'Escape',    ctrlKey: true,  altKey: true,  shiftKey: true  }),
+    profile:        new KeyboardEvent('keydown', { key: 'p',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    speedUp:        new KeyboardEvent('keydown', { key: '.',         ctrlKey: false, altKey: false, shiftKey: true  }),
+    speedDown:      new KeyboardEvent('keydown', { key: ',',         ctrlKey: false, altKey: false, shiftKey: true  }),
+    settings:       new KeyboardEvent('keydown', { key: ',',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    emojiPanel:     new KeyboardEvent('keydown', { key: 'e',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    gifPanel:       new KeyboardEvent('keydown', { key: 'g',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    stickerPanel:   new KeyboardEvent('keydown', { key: 's',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+    extendedSearch: new KeyboardEvent('keydown', { key: 'k',         ctrlKey: false, altKey: true,  shiftKey: false }),
+    lockScreen:     new KeyboardEvent('keydown', { key: 'l',         ctrlKey: true,  altKey: true,  shiftKey: false }),
+}
 
-const prevChatEvent =
-    new KeyboardEvent('keydown', {
-        key: 'Tab',
-        ctrlKey: true,
-        altKey: true,
-        shiftKey: true
-    });
-
+function tryIntoEvent({key, ctrlKey, altKey, shiftKey}) {
+    const shortcutKey = `${key.toLowerCase()}:${ctrlKey}:${altKey}:${shiftKey}`
+    return commands?.[hashedDefinitions?.[shortcutKey]] 
+}
 
 // Main key event handler
 document.addEventListener('keydown', (event) => {
-  // Check for 'next chat'
-  if (event.key === nextChatShortcut.key &&
-      event.ctrlKey === nextChatShortcut.ctrl &&
-      event.altKey === nextChatShortcut.alt &&
-      event.shiftKey === nextChatShortcut.shift) {
-      document.dispatchEvent(nextChatEvent)
+    const newEvent = tryIntoEvent(event)
+    if (newEvent) {
+      document.dispatchEvent(newEvent)
       event.preventDefault()
       event.stopImmediatePropagation()
-  }
-  // Check for 'previous chat'
-  else if (event.key === prevChatShortcut.key &&
-           event.ctrlKey === prevChatShortcut.ctrl &&
-           event.altKey === prevChatShortcut.alt &&
-           event.shiftKey === prevChatShortcut.shift) {
-      document.dispatchEvent(prevChatEvent)
-      event.preventDefault()
-      event.stopImmediatePropagation()
-  }
+    }
 }, true);
--- js/options.js
+++ js/options.js
@@ -1,38 +1,109 @@
+const defaultCommands = {
+  delete:         { key: 'Backspace', ctrl: true,  alt: true,  shift: true  },
+  markUnread:     { key: 'u',         ctrl: true,  alt: true,  shift: true  },
+  mute:           { key: 'm',         ctrl: true,  alt: true,  shift: true  },
+  archive:        { key: 'e',         ctrl: true,  alt: true,  shift: true  },
+  pin:            { key: 'p',         ctrl: true,  alt: true,  shift: true  },
+  search:         { key: '/',         ctrl: true,  alt: true,  shift: false },
+  searchInChat:   { key: 'f',         ctrl: true,  alt: true,  shift: true  },
+  newChat:        { key: 'n',         ctrl: true,  alt: true,  shift: false },
+  newGroup:       { key: 'n',         ctrl: true,  alt: true,  shift: true  },
+  nextChat:       { key: 'Tab',       ctrl: true,  alt: true,  shift: false },
+  prevChat:       { key: 'Tab',       ctrl: true,  alt: true,  shift: true  },
+  closeChat:      { key: 'Escape',    ctrl: true,  alt: true,  shift: true  },
+  profile:        { key: 'p',         ctrl: true,  alt: true,  shift: false },
+  speedUp:        { key: '.',         ctrl: false, alt: false, shift: true  },
+  speedDown:      { key: ',',         ctrl: false, alt: false, shift: true  },
+  settings:       { key: ',',         ctrl: true,  alt: true,  shift: false },
+  emojiPanel:     { key: 'e',         ctrl: true,  alt: true,  shift: false },
+  gifPanel:       { key: 'g',         ctrl: true,  alt: true,  shift: false },
+  stickerPanel:   { key: 's',         ctrl: true,  alt: true,  shift: false },
+  extendedSearch: { key: 'k',         ctrl: false, alt: true,  shift: false },
+  lockScreen:     { key: 'l',         ctrl: true,  alt: true,  shift: false },
+}
+
+const commandKeys = Object.keys(defaultCommands)
+
+function createShortcutForm(cmd) {
+  const keyInput = document.createElement('input')
+  keyInput.id = `key-${cmd}`
+  const keyLabel = document.createElement('label')
+  keyLabel.htmlFor = keyInput.id
+  keyInput.type = 'text'
+  keyLabel.innerText = cmd
+
+  const ctrlLabel = document.createElement('label')
+  ctrlLabel.innerText = 'Ctrl'
+  const ctrlInput = document.createElement('input')
+  ctrlInput.type = 'checkbox'
+  ctrlInput.id = `ctrl-${cmd}`
+  ctrlLabel.appendChild(ctrlInput)
+
+  const altLabel = document.createElement('label')
+  altLabel.innerText = 'Alt'
+  const altInput = document.createElement('input')
+  altInput.type = 'checkbox'
+  altInput.id = `alt-${cmd}`
+  altLabel.appendChild(altInput)
+
+  const shiftLabel = document.createElement('label')
+  shiftLabel.innerText = 'Shift'
+  const shiftInput = document.createElement('input')
+  shiftInput.type = 'checkbox'
+  shiftInput.id = `shift-${cmd}`
+  shiftLabel.appendChild(shiftInput)
+
+  const wrapper = document.createElement('div')
+  wrapper.id = `wrapper-${cmd}`
+  wrapper.appendChild(keyLabel)
+  wrapper.appendChild(keyInput)
+  wrapper.appendChild(ctrlLabel)
+  wrapper.appendChild(altLabel)
+  wrapper.appendChild(shiftLabel)
+
+  return wrapper
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  // Load current settings
-  browser.storage.local.get(['nextChatShortcut', 'prevChatShortcut'])
-  .then((result) => {
-    if (result.nextChatShortcut) {
-      document.getElementById('nextChatKey').value = result.nextChatShortcut.key;
-      document.getElementById('nextCtrl').checked = result.nextChatShortcut.ctrl;
-      document.getElementById('nextAlt').checked = result.nextChatShortcut.alt;
-      document.getElementById('nextShift').checked = result.nextChatShortcut.shift;
-    }
-    if (result.prevChatShortcut) {
-      document.getElementById('prevChatKey').value = result.prevChatShortcut.key;
-      document.getElementById('prevCtrl').checked = result.prevChatShortcut.ctrl;
-      document.getElementById('prevAlt').checked = result.prevChatShortcut.alt;
-      document.getElementById('prevShift').checked = result.prevChatShortcut.shift;
-    }
+  // Build form
+  const container = document.getElementById('container')
+  commandKeys.forEach(cmd => container.appendChild(createShortcutForm(cmd)))
+
+  // Load current definitions from storage
+  browser.storage.local.get(['definitions']).then((result) => {
+    Object.entries(defaultCommands).forEach(([cmd, params]) => {
+      if (result.definitions?.[cmd]) {
+        setValuesForCmd(cmd, result.definitions[cmd])
+      } else {
+        setValuesForCmd(cmd, params)
+      }
+    })
   });
 
-  // Save settings
+  // Save new definitions
   document.getElementById('saveBtn').addEventListener('click', () => {
-    const nextChatShortcut = {
-      key: document.getElementById('nextChatKey').value,
-      ctrl: document.getElementById('nextCtrl').checked,
-      alt: document.getElementById('nextAlt').checked,
-      shift: document.getElementById('nextShift').checked
-    };
+    const definitions = {}
+    const hashedDefinitions = {}
+    commandKeys.forEach(cmd => {
+      const obj = {
+        key: document.getElementById(`key-${cmd}`).value,
+        ctrl: document.getElementById(`ctrl-${cmd}`).checked,
+        alt: document.getElementById(`alt-${cmd}`).checked,
+        shift: document.getElementById(`shift-${cmd}`).checked,
+      }
+      definitions[cmd] = obj
+      const hash = `${obj.key}:${obj.ctrl}:${obj.alt}:${obj.shift}`
+      hashedDefinitions[hash] = cmd
+    })
 
-    const prevChatShortcut = {
-      key: document.getElementById('prevChatKey').value,
-      ctrl: document.getElementById('prevCtrl').checked,
-      alt: document.getElementById('prevAlt').checked,
-      shift: document.getElementById('prevShift').checked
-    };
-
-    browser.storage.local.set({nextChatShortcut, prevChatShortcut});
+    browser.storage.local.set({definitions, hashedDefinitions});
   });
 });
+
+function setValuesForCmd(command, params) {
+    document.getElementById(`key-${command}`).value = params.key
+    document.getElementById(`ctrl-${command}`).checked = params.ctrl
+    document.getElementById(`alt-${command}`).checked = params.alt
+    document.getElementById(`shift-${command}`).checked = params.shift
+}
 
